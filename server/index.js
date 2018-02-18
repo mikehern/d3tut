ae908ce27e39766621ec4fe5531ac17f72dae12e
Mike Hernandez <mkhern@gmail.com>
2018-02-17 19:06:46 -0800

    Downloads data via Google Trends.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,38 @@
-const express = require('express')
-const app = express()
-const d3 = require('d3')
+const express = require('express');
+const app = express();
+const d3 = require('d3');
+const googleTrends = require('google-trends-api');
+const fs = require('fs');
 
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Test!'))
+app.get('/', (req, res) => res.send('Test!'));
 
-app.use('/l1', express.static('./lessons/1/index.html'))
-app.use('/l2', express.static('./lessons/2/index.html'))
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+const saveOutput = (queryType, queryResults) => {
+  fs.writeFile(`./tmp/${queryType}.txt`, queryResults, null, () => console.log('Completed write!')
+)};
+
+let query = {keyword: 'blockchain'};
+
+async function collectData() {
+  let geoResult = await googleTrends.interestByRegion(query);
+  await saveOutput('geo', geoResult);
+  await delay(3000);
+  let timeResult = await googleTrends.interestOverTime(query);
+  await saveOutput('time', timeResult);
+  await delay(3000);
+  let autoCompleteResult = await googleTrends.autoComplete(query);
+  await saveOutput('autoComplete', autoCompleteResult);
+  await delay(3000);
+  let relatedQueriesResult = await googleTrends.relatedQueries(query);
+  await saveOutput('relatedQueries', relatedQueriesResult);
+  await delay(3000);
+  let relatedTopicsResult = await googleTrends.relatedTopics(query);
+  await saveOutput('relatedTopics', relatedTopicsResult);
+}
+
+collectData().catch(err => console.error(err));
